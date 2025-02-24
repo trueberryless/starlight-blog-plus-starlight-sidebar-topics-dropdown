399a642bcf585475577f22a1b6210961ef65b0b8
trueberryless <trueberryless+github@gmail.com>
2025-02-24 18:05:39 +0100

    delete try/catch block (not working)

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,10 @@
 import { defineRouteMiddleware } from "@astrojs/starlight/route-data";
 
 export const onRequest = defineRouteMiddleware(async (context, next) => {
-  try {
-    const { entry, id } = context.locals.starlightRoute;
+  const { entry, id } = context.locals.starlightRoute;
 
-    if (id.startsWith("blog")) {
-      entry.data.topic = "blog";
-    }
-  } catch (e) {}
+  if (id.startsWith("blog")) {
+    entry.data.topic = "blog";
+  }
   return next();
 });
